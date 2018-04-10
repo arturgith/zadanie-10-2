39ec3a205f15b9b4459c74d1ad0565c2d3708f77
Artur Marcinkowski <arturmarcinkowski@interia.pl>
2018-04-10 15:26:32 +0200

    add code to js file

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 $(function() {
 
-    var span = $('span');
-        span.each(function(index, element) {
-            if(index % 2 == 0) {
-            $(element).css('color', 'red');
+   var span = $('span');
+    span.each(function(index, element) {
+        if(index % 2 == 0) {
+        $(element).css('color', 'red');
+
+    //Można też wykonać za pomoca:
+    //$('span:even').css('color', 'red');
         };
     });
-    
+    var paragraphs = $('p');
+    paragraphs.each(function(index, element) {
+        var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+        $(element).append(button);
+        $('button').click(function(){
+            alert($(this).attr('data-tmp'));
+        });
+    });
 });
