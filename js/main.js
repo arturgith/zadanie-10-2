e1ebbf755cef2866350d793c9d5ef8d941568c73
Artur Marcinkowski <arturmarcinkowski@interia.pl>
2018-04-10 15:24:13 +0200

    add files and code to exercise 10.2

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    var span = $('span');
+        span.each(function(index, element) {
+            if(index % 2 == 0) {
+            $(element).css('color', 'red');
+        };
+    });
+    
+});
